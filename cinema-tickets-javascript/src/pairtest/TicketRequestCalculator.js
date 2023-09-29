3f7dd1397378b5d89450e57bf2314383eae61e81
William Falconer (MOJ) <william.falconer@Williams-MacBook-Pro.local>
2023-09-30 00:22:59 +0100

    refactor no of tickets to variable

--- cinema-tickets-javascript/src/pairtest/TicketRequestCalculator.js
+++ cinema-tickets-javascript/src/pairtest/TicketRequestCalculator.js
@@ -13,8 +13,9 @@ import TicketCalculatorResult  from "./lib/TicketCalculatorResult.js";
         if (request == null) throw new Error();
 
         var result = new TicketCalculatorResult();
+        var totaltickets = request.getNoOfTickets();
         // get it working for the single case
-        if (request.getNoOfTickets() < 1 || request.getNoOfTickets() > 20){
+        if (totaltickets < 1 || totaltickets > 20){
             result.messages.push("please request between 1 and 20 tickets"); // possibly constant or even resource string this out.
         }
 
